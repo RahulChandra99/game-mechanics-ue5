5111196238f2edcfc736278d43e6227cb926f21e
Rahul Chandra <60950973+RahulChandra99@users.noreply.github.com>
2024-10-22 00:15:22 +0100

    fighter game side scroller class setup

--- Source/GameplayMechanics/Private/FightingGame/FighterCharacter.cpp
+++ Source/GameplayMechanics/Private/FightingGame/FighterCharacter.cpp
@@ -3,12 +3,53 @@
 
 #include "FightingGame/FighterCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+
 // Sets default values
 AFighterCharacter::AFighterCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	GetCapsuleComponent()->InitCapsuleSize(42.f,96.f);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+
+	// Create a camera boom attached to the root (capsule)
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+
+	CameraBoom->bUsePawnControlRotation = false; // If you don't want the camera boom to rotate based on pawn control rotation
+	CameraBoom->bInheritPitch = false;
+	CameraBoom->bInheritYaw = false;
+	CameraBoom->bInheritRoll = false;
+	CameraBoom->bDoCollisionTest = false;
+	CameraBoom->TargetArmLength = 500.f;
+	CameraBoom->SocketOffset = FVector(0.f, 0.f, 75.f);
+	CameraBoom->SetRelativeRotation(FRotator(0.f, 180.f, 0.f));
+	
+
+	// Create a camera and attach to boom
+	SideViewCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("SideViewCamera"));
+	SideViewCameraComponent->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	SideViewCameraComponent->bUsePawnControlRotation = false; // We don't want the controller rotating the camera
+
+	// Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = true; // Face in the direction we are moving
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 720.0f, 0.0f); // ...at this rotation rate
+	GetCharacterMovement()->GravityScale = 2.f;
+	GetCharacterMovement()->AirControl = 0.80f;
+	GetCharacterMovement()->JumpZVelocity = 1000.f;
+	GetCharacterMovement()->GroundFriction = 3.f;
+	GetCharacterMovement()->MaxWalkSpeed = 600.f;
+	GetCharacterMovement()->MaxFlySpeed = 600.f;
+	
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +57,11 @@ void AFighterCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void AFighterCharacter::MoveRight(float Value)
+{
+	AddMovementInput(FVector(0.f,-1.f,0.f), Value);
 }
 
 // Called every frame
@@ -30,5 +76,27 @@ void AFighterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAction("Jump",IE_Pressed,this,&ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump",IE_Released,this,&ACharacter::Jump);
+
+	PlayerInputComponent->BindAxis("MoveRight",this,&AFighterCharacter::MoveRight);
+
 }
+
+void AFighterCharacter::StartAttack1()
+{
+}
+
+void AFighterCharacter::StartAttack2()
+{
+}
+
+void AFighterCharacter::StartAttack3()
+{
+}
+
+void AFighterCharacter::StartAttack4()
+{
+}
+
 
--- Source/GameplayMechanics/Public/FightingGame/FighterCharacter.h
+++ Source/GameplayMechanics/Public/FightingGame/FighterCharacter.h
@@ -11,6 +11,19 @@ class GAMEPLAYMECHANICS_API AFighterCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+private:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= Camera, meta =(AllowPrivateAccess = "true"))
+	class UCameraComponent* SideViewCameraComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= Camera, meta =(AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	void StartAttack1();
+	void StartAttack2();
+	void StartAttack3();
+	void StartAttack4();
+
 public:
 	// Sets default values for this character's properties
 	AFighterCharacter();
@@ -18,6 +31,9 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	void MoveRight(float Value);
+	
 
 public:	
 	// Called every frame
