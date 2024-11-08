fc265bdc085848bb648493094bfbd5af93855922
Rahul Chandra <60950973+RahulChandra99@users.noreply.github.com>
2024-11-08 17:37:31 +0530

    rpg camera setup

--- Source/GameplayMechanics/Private/RPG/Characters/RPGHeroCharacter.cpp
+++ Source/GameplayMechanics/Private/RPG/Characters/RPGHeroCharacter.cpp
@@ -1,5 +1,52 @@
 // Rahul Chandra All Rights Reserved
 
-
 #include "RPG/Characters/RPGHeroCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+ARPGHeroCharacter::ARPGHeroCharacter()
+{
+	// Initialize CameraBoom
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 200.f;
+	CameraBoom->SocketOffset = FVector(0.f, 55.f, 65.f);
+	CameraBoom->bUsePawnControlRotation = true;
+
+	// Initialize FollowCamera
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom);
+	FollowCamera->bUsePawnControlRotation = false;
+
+	// Initialize CapsuleComponent
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.f);
+
+	// Configure character rotation behavior
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+
+	// Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 500.f, 0.f);
+	GetCharacterMovement()->MaxWalkSpeed = 400.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+}
+
+void ARPGHeroCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ARPGHeroCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ARPGHeroCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
--- Source/GameplayMechanics/Public/RPG/Characters/RPGHeroCharacter.h
+++ Source/GameplayMechanics/Public/RPG/Characters/RPGHeroCharacter.h
@@ -6,12 +6,33 @@
 #include "RPG/Characters/RPGBaseCharacter.h"
 #include "RPGHeroCharacter.generated.h"
 
-/**
- * 
- */
+class USpringArmComponent;
+class UCameraComponent;
+
+
 UCLASS()
 class GAMEPLAYMECHANICS_API ARPGHeroCharacter : public ARPGBaseCharacter
 {
 	GENERATED_BODY()
+
+private:
+
+	ARPGHeroCharacter();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera",meta = (AllowPrivateAccess))
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera",meta = (AllowPrivateAccess))
+	UCameraComponent* FollowCamera;
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	
 };
